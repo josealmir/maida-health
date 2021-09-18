84856b773aaa2e7b344996fcda73851408ba30e1
Jose Almir <almir2560@gmail.com>
2021-09-17 22:31:34 -0300

    Configuracao de deploy no heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  viacep: "https://viacep.com.br/ws/",
+  googlemaps:"https://maps.googleapis.com/maps/api/geocode/json?address=",
+  googlemapskey: "AIzaSyBzwbD1WErqzCLa6J50nzkQoJqZCMfgscE"
 };
